97f41baad18a0a915a94142d397a5547c7cf02cd
Clint <clintthomas@pm.me>
2020-12-07 14:47:03 +0800

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Clint Thomas on 7/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
